85969f55181256d985225d5d3777f19cf748bf1b
Roxanne Baldwin <rox.m.baldwin@gmail.com>
2017-09-16 14:55:48 -0600

    🤡  added a local seed

---
+++ container/data/seed.js
@@ -0,0 +1,49 @@
+const json = [
+  {
+    "id": 1,
+    "comment": "where is mike? we miss him sooooo",
+    "image": "https://edanwebster.files.wordpress.com/2013/05/hangover-cat.jpg",
+    "loc": {
+      x: -73.974,
+      y: 40.764
+    },
+    "selected": true
+  },
+  {
+    "id": 2,
+    "comment": "Mike found a friend",
+    "image": "https://i.pinimg.com/736x/21/74/8c/21748c1cc0de9df09715de622bfdfbf3.jpg",
+    "loc": {
+      x: -53.974,
+      y: 20.764
+    }
+  },
+  {
+    "id": 3,
+    "comment": "Mike has an adventure",
+    "image": "http://i0.wp.com/peopledotcom.files.wordpress.com/2017/05/adventure-cats-1.jpg?crop=0px,0px,1000px,525px&resize=1200,630&ssl=1",
+    "loc": {
+      x: 73.974,
+      y: -40.764
+    },
+    "selected": true
+  },
+  {
+    "id": 4,
+    "comment": "Mike got lost."
+    "image": "http://www.funnycatsite.com/pictures/I_Am_Flour.jpg",
+    "loc": {
+      x: -76.974,
+      y: 40.764
+    },
+    "selected": true
+  },
+  {
+    "id": 5,
+    "comment": "Mike is found!",
+    "image": "http://img.chan4chan.com/img/2009-04-25/1240639425926.jpg",
+    "loc": {
+      x: -79.974,
+      y: 70.764
+    },
+  }
