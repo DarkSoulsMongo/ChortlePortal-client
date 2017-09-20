9859668a01f3d746a99ea87f7fb90426b49982ab
Michael Harrington <mike85h@gmail.com>
2017-09-20 09:46:00 -0600

    bug in map geojson file fixed. all working now as it was before last merge issue

--- container/application/application.component.js
+++ container/application/application.component.js
@@ -9,9 +9,9 @@
         // console.log(vm)
         vm.$onInit = function(){
           const url = 'https://salty-mountain-21631.herokuapp.com/';
-          $http.get(url).then(function(data) {
-            vm.data = data.data;
-          })
+          // $http.get(url).then(function(data) {
+          //   vm.data = data.data;
+          // })
            vm.formstatus = {
              view: true
            }
